598e51935d866c48554f5f29bb45965b4e1b624b
yawn <yawning055@gmail.com>
2023-09-16 15:37:06 +0900

    Redis 관련 클래스 작성 완료
    https://www.daddyprogrammer.org/post/4691/spring-websocket-chatting-server-stomp-server/
    
    확장프로그램: https://chrome.google.com/webstore/detail/websocket-test-client/fgponpodhbmadfljofbimhhlengambbn
    
    주소: ws://localhost:8080/ws/chat

---
+++ src/main/java/com/chatting/chat/pubsub/RedisPublisher.java
@@ -0,0 +1,23 @@
+package com.chatting.chat.pubsub;
+
+import com.chatting.chat.dto.ChatMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Service;
+
+import java.nio.channels.Channel;
+
+/**
+ * 채팅방에 입장하여 메시지를 작성하면 해당 메시지를 Redis Topic에 발행하는 기능의 서비스
+ * 이 서비스를 통해 메시지를 발행하면 대기하고 있던 redis 구독 서비스가 메시지를 처리한다
+ */
+@RequiredArgsConstructor
+@Service
+public class RedisPublisher {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void publish(ChannelTopic topic, ChatMessage message) {
+        redisTemplate.convertAndSend(topic.getTopic(), message);
+    }
+}
